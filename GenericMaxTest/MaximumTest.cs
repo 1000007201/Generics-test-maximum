a5c5481c363352af66022685c79d1da9c688a1b6
1000007201 <nishant.sharma5504@gmail.com>
2022-02-09 23:26:40 +0530

    [Nishant] Add:Test case 1.1

--- GenericMaxTest/MaximumTest.cs
+++ GenericMaxTest/MaximumTest.cs
@@ -10,7 +10,7 @@ namespace GenericMaxTest
         public void GivenIntegerValue_WhenChech_ReturnMaximum()
         {
             MaximumOfThese max = new MaximumOfThese();
-            double data = max.Max(1.1, 3.1, 2.1);
+            double data = max.Max(3.1,1.1,2.1);
             Assert.AreEqual(3.1,data);
         }
     }
